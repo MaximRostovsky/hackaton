62b54063a7793b01089df2f8c1c7abfb357c1e68
Maxim Rostovsky <nobleswank@gmail.com>
2018-05-06 01:11:52 +0500

    add frontend v1

---
+++ Page.js
@@ -0,0 +1,30 @@
+function UploadPhoto() {
+    file = document.getElementById('Photo').files[0];
+    var output = [];
+    output.push("File ", file.name, " was uploaded successfully");
+    document.getElementById('FileChosen').innerHTML = '<ul>' + output.join('') + '</ul>';
+}
+
+function sendToServ() {
+    alert("Sending to server");
+    var objectToSend = {
+        FIO: document.getElementById('FIO').value,
+        Phone: document.getElementById('Phone').value,
+        Photo: file
+    };
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://127.0.0.1:5000/json", true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var json;
+        }
+    }
+    xhr.send(JSON.stringify(objectToSend));
+}
+
+var file;
+window.onload = function () {
+    var photo = document.getElementById('Photo');
+    document.getElementById('Photo').addEventListener('change', UploadPhoto, false);
+}
--- Serv.py
+++ Serv.py
@@ -1,21 +1,29 @@
 
 # coding: utf-8
 
-# In[14]:
+# In[19]:
 
 
 from flask import Flask;
+from flask import render_template;
 app = Flask(__name__)
 
 
-# In[15]:
+# In[20]:
 
 
 @app.route("/")
 def startServer():
-    return "Hello";
+    return render_template("Page.html");
 
 # На анализ
+@app.route("/json", methods = ['POST'])
 def goDeeper(inJSON):
     return;
+
+
+# In[17]:
+
+
+
 
